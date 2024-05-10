2430244d460fc87e0c90ea8dda63344713437bb2
Paul Garren <santiago.paul.garren@gmail.com>
2024-05-11 00:19:49 +0800

    commit "version database"

---
+++ app/src/main/java/com/labactivity/flashcarddesign/LoginScreen.kt
@@ -0,0 +1,67 @@
+package com.labactivity.flashcarddesign
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.labactivity.flashcarddesign.databinding.LoginScreenBinding
+
+class LoginScreen : AppCompatActivity() {
+    private lateinit var binding:LoginScreenBinding
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = LoginScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.signUpTxt.setOnClickListener(){
+            val intent = Intent(this,SignIn::class.java)
+            startActivity(intent)
+            this.finish()
+        }
+
+        binding.LoginBtn.setOnClickListener {
+            val emailInput = binding.emailEditText.text.toString()
+            val passwordInput = binding.PasswordTxt.text.toString()
+            val progressBar = binding.progressBar
+            auth = Firebase.auth
+
+            progressBar.visibility = View.VISIBLE
+
+            if (TextUtils.isEmpty(emailInput) || TextUtils.isEmpty(passwordInput)) {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+                progressBar.visibility = View.GONE
+                return@setOnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(emailInput, passwordInput)
+                .addOnCompleteListener(this) { task ->
+                    progressBar.visibility = View.GONE
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this,
+                            "Login successful.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            this,
+                            "Authentication failed: ${task.exception?.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+}
---
+++ app/src/main/java/com/labactivity/flashcarddesign/SignIn.kt
@@ -0,0 +1,62 @@
+package com.labactivity.flashcarddesign
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.labactivity.flashcarddesign.databinding.SignInBinding
+
+class SignIn : AppCompatActivity() {
+    private lateinit var binding:SignInBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = SignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.SignUpBtn.setOnClickListener {
+            val usernameInput = binding.usernameTxt.text.toString()
+            val emailInput = binding.EmailAddTxt.text.toString() // Use text property instead of toString()
+            val passwordInput = binding.PasswordTxt.text.toString() // Use text property instead of toString()
+            val progressBar = binding.progressBar
+            auth = Firebase.auth
+
+            progressBar.visibility = View.VISIBLE
+
+            if (TextUtils.isEmpty(emailInput) || TextUtils.isEmpty(passwordInput) || TextUtils.isEmpty(usernameInput)) {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+                progressBar.visibility = View.GONE
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(emailInput, passwordInput)
+                .addOnCompleteListener(this) { task ->
+                    progressBar.visibility = View.GONE
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this,
+                            "Successfully Registered.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        val intent = Intent(this, LoginScreen::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            this,
+                            "Authentication failed: ${task.exception?.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+
+    }
+}
