541c5d776c34f95c3c8f00f8ea940265c89ff1a4
Louie <arangorinjustinlouie@gmail.com>
2024-05-13 18:07:07 +0800

    Version Louie

---
+++ app/src/main/java/com/labactivity/flashcarddesign/AboutUs.kt
@@ -0,0 +1,31 @@
+package com.labactivity.flashcarddesign
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.labactivity.flashcarddesign.databinding.ActivityAboutUsBinding
+import com.labactivity.flashcarddesign.databinding.LoginScreenBinding
+
+class AboutUs : AppCompatActivity() {
+    private lateinit var binding: ActivityAboutUsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutUsBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnLogin.setOnClickListener() {
+            val intent = Intent(this, LoginScreen::class.java)
+            startActivity(intent)
+            this.finish()
+        }
+    }
+}
